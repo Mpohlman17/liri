06f81d5e09b19ff5f69c9e0b0757a31a1e4b3d13
Michael Pohlman <michael.pohlman1@gmail.com>
2018-11-29 17:23:11 -0500

    spotify and bands intown working, omdb movie is only loading partial data

--- liri.js
+++ liri.js
@@ -26,38 +26,50 @@ function switchCase() {
             break;
     }
 };
+switchCase();
+/**
+ * bands in town
+ * the input is manully input, so it will show you 
+ * the venue for the bsnd The Sign
+ */
 
-// function concert(inputs) {
+function concert(inputs) {
 
-//     var bandUrl = "https://rest.bandsintown.com/artists/" + inputs + "/events?app_id=codingbootcamp"
-// if (!inputs){
-//     inputs = 'The Sign';
-// }
-// request(queryUrl, function (error, response, body) {
+    const bandUrl = "https://rest.bandsintown.com/artists/" + inputs + "/events?app_id=codingbootcamp"
+    if (!inputs) {
+        inputs = 'The Sign';
+    }
+    request(bandUrl, function (error, response, body) {
 
-//     if (!error && response.statusCode === 200) {
+        if (!error && response.statusCode === 200) {
 
-//         var JS = JSON.parse(body);
-//         for (i = 0; i < JS.length; i++) {
-//             var dTime = JS[i].datetime;
-//             var month = dTime.substring(5, 7);
-//             var year = dTime.substring(0, 4);
-//             var day = dTime.substring(8, 10);
-//             var dateFormat = (month + "/" + day + "/" + year);
-//             // console.log("Venue Name: " + JS[i].venue.name);
-//             // console.log("Location: "JS[i].venue.city);
-//             // console.log("Date: " + dateFormat);
+            var event = JSON.parse(body);
+            for (i = 0; i < event.length; i++) {
+                var dTime = event[i].datetime;
+                var month = dTime.substring(5, 7);
+                var year = dTime.substring(0, 4);
+                var day = dTime.substring(8, 10);
+                var dateFormat = (month + "/" + day + "/" + year);
+                console.log("Venue Name: " + event[i].venue.name);
+                console.log("City: " + event[i].venue.city);
+                console.log("Date: " + dateFormat);
 
-//             // }
-//         }
-//     }
-// })
+            }
+        }
+    })
+}
+
+/**
+ * spotify
+ * the input is manully input, so it will show you 
+ * the song information for I Want It That Way by The Backstreet Boys
+ */
 
 function spotify(inputs) {
 
-    var spotify = new Spotify(keys.spotifyKeys);
+    const spotify = new Spotify(keys.spotify);
     if (!inputs) {
-        inputs = 'Fractures';
+        inputs = 'I Want It That Way';
     }
     spotify.search({
         type: 'track',
@@ -67,44 +79,41 @@ function spotify(inputs) {
             console.log('Error occurred: ' + err);
             return;
         }
-        console.log(data);
 
-        // var songInfo = data.tracks.items;
-        // console.log("Artist(s): " + songInfo[0].artists[0].name);
-        // console.log("Song Name: " + songInfo[0].name);
-        // console.log("Preview Link: " + songInfo[0].preview_url);
-        // console.log("Album: " + songInfo[0].album.name);
+        const songInfo = data.tracks.items;
+        console.log("Artist(s): " + songInfo[0].artists[0].name);
+        console.log("Song Name: " + songInfo[0].name);
+        console.log("Preview Link: " + songInfo[0].preview_url);
+        console.log("Album: " + songInfo[0].album.name);
     });
-    var songInfo = data.tracks.items;
-    console.log("Artist(s): " + songInfo[0].artists[0].name);
-    console.log("Song Name: " + songInfo[0].name);
-    console.log("Preview Link: " + songInfo[0].preview_url);
-    console.log("Album: " + songInfo[0].album.name);
 }
-spotify();
 
+/**
+ * ombd movie reviews
+ * the input is manully input, so it will show you 
+ * title, release year, ratings, country, language, plot, and actors
+ */
+function movie(inputs) {
 
-// function movie(inputs) {
+    const queryUrl = "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=trilogy";
 
-//     var queryUrl = "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=d5be8b20";
+    request(queryUrl, function (error, response, body) {
+        if (!inputs) {
+            inputs = 'Mr. Nobody';
+        }
+        if (!error && response.statusCode === 200) {
 
-//     request(queryUrl, function (error, response, body) {
-//         if (!inputs) {
-//             inputs = 'Mr Nobody';
-//         }
-//         if (!error && response.statusCode === 200) {
-
-//             console.log("Title: " + JSON.parse(body).Title);
-//             console.log("Release Year: " + JSON.parse(body).Year);
-//             console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-//             console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-//             console.log("Country: " + JSON.parse(body).Country);
-//             console.log("Language: " + JSON.parse(body).Language);
-//             console.log("Plot: " + JSON.parse(body).Plot);
-//             console.log("Actors: " + JSON.parse(body).Actors);
-//         }
-//     });
-// };
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1]);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+};
 
 function whatItSays() {
     fs.readFile('random.txt', "utf8", function (error, data) {
@@ -118,7 +127,7 @@ function whatItSays() {
             var songcheck = dataArr[1].slice(1, -1);
             spotify(songcheck);
         } else if (dataArr[0] === "concert-this") {
-            if (dataArr[1].charAt(1) === "'") {
+            if (dataArr[1].charAt(1) === "") {
                 var dLength = dataArr[1].length - 1;
                 var data = dataArr[1].substring(2, dLength);
                 console.log(data);
@@ -134,4 +143,8 @@ function whatItSays() {
         }
     });
 }
-switchCase()
+// calling functions
+
+// concert();
+// movie();
+// whatItSays();
